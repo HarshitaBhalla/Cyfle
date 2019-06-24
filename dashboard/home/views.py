18f2ac31f4e2625f39c847968ddcad8ba7effa3e
rishitsahu64 <rishitsahu64@gmail.com>
2019-06-25 03:01:55 +0530

    updated

--- dashboard/home/views.py
+++ dashboard/home/views.py
@@ -4,5 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-def login(request):
-    return render(request,'login.html')
