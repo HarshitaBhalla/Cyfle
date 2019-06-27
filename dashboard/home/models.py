66a97d5a2943246a8087648fb5a7c9108199221f
sarthakg04 <sarthakanamika@gmail.com>
2019-06-27 20:21:55 +0530

    anshu check kar yaar :(

--- dashboard/home/models.py
+++ dashboard/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username= models.TextField()
+    name= models.CharField(max_length=20)
+    img= models.ImageField(upload_to='pics')
+    jobsa= models.TextField()
+
+    
--- dashboard/home/views.py
+++ dashboard/home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    dest1= User()
+    return render(request,'index.html',{'dest1':dest1})
+    
+
 def login(request):
     return render(request,'login.html')
